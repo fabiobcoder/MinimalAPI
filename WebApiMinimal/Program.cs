af766815312778bee3d6c10bc0f87716b5bdb064
Fabio <fabio@ifabio.com.br>
2022-11-10 22:41:34 -0300

    MinimalAPi

---
+++ WebApiMinimal/Program.cs
@@ -0,0 +1,54 @@
+using Microsoft.EntityFrameworkCore;
+using WebApiMinimal.Contexto;
+using WebApiMinimal.Models;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddDbContext<Contexto>
+    (options => options.UseSqlServer(
+        "Data Source=FABIO-PC;" +
+        "Initial Catalog=MINIMA_API_AULA;" +
+        "Integrated Security=False;" +
+        "User ID=sa;" +
+        "Password=mudar@123;" +
+        "Connect Timeout=15;" +
+        "Encrypt=False;" +
+        "TrustServerCertificate=False"));
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+app.UseSwagger();
+
+app.MapPost("AdicionaProduto", async (Produto produto, Contexto contexto) =>
+{
+    contexto.Produto.Add(produto);
+    await contexto.SaveChangesAsync();
+});
+
+app.MapPost("ExcluirProduto/{id}", async (int id, Contexto contexto) =>
+{
+    var produtoExcluir = await contexto.Produto.FirstOrDefaultAsync(p => p.Id == id);
+    if (produtoExcluir != null)
+    {
+        contexto.Produto.Remove(produtoExcluir);
+        await contexto.SaveChangesAsync();
+    }
+});
+
+app.MapPost("ListarProdutos", async (Contexto contexto) =>
+{
+    return await contexto.Produto.ToListAsync();
+});
+
+app.MapPost("ObterProduto/{id}", async (int id, Contexto contexto) =>
+{
+    return await contexto.Produto.FirstOrDefaultAsync(p => p.Id == id);
+});
+
+
+app.UseSwaggerUI();
+app.Run();
